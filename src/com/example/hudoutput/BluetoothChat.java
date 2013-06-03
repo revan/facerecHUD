1555b4764c9d7953b5fe46e02571c8d1eb063c0d
Revan Sopher <rsopher@gmail.com>
2013-06-03 14:01:51 -0400

    don't display sending device name in messages

--- src/com/example/hudoutput/BluetoothChat.java
+++ src/com/example/hudoutput/BluetoothChat.java
@@ -254,7 +254,7 @@ public class BluetoothChat extends Activity {
                 byte[] readBuf = (byte[]) msg.obj;
                 // construct a string from the valid bytes in the buffer
                 String readMessage = new String(readBuf, 0, msg.arg1);
-                mConversationArrayAdapter.add(mConnectedDeviceName+":  " + readMessage);
+                mConversationArrayAdapter.add(readMessage);
                 break;
             case MESSAGE_DEVICE_NAME:
                 // save the connected device's name
